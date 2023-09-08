050bcd09fec6f69a8d54d03aaea0e064bc08d17f
Ajay <ajayluhach4@gmail.com>
2023-09-08 12:52:29 +0530

    h

--- src/config.py
+++ src/config.py
@@ -1,2 +1,2 @@
-SQLALCHEMY_DATABASE_URI = 'postgresql://ajay:ajay@localhost:5800/todo'
+SQLALCHEMY_DATABASE_URI = 'postgresql://ajay:ajay@127.0.0.1:5800/todo'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
